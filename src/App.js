c9156bfcf354e0dc37b75a1fbb37f7b965e08366
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-11-03 23:21:32 +0600

    Route set

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import {
   BrowserRouter as Router, Route, Switch
 } from "react-router-dom";
 import './App.css';
+import Home from "./Pages/Home/Home/Home";
+import Footer from "./Pages/Shared/Footer/Footer";
+import Header from "./Pages/Shared/Header/Header";
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        
+        <Header></Header>
         <Switch>
-          <Route>
-          
+          <Route exact path='/'>
+            <Home></Home>
+          </Route>
+          <Route path='/home'>
+            <Home></Home>
           </Route>
         </Switch>
-        
+        <Footer></Footer>
       </Router>
     </div>
   );
